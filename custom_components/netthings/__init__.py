60a70d13f6ee2ba5d64bd1dc70a4a626d15f2e1c
Scott Stammers <57189524+stammersscott@users.noreply.github.com>
2021-08-15 15:56:34 +0100

    Update __init__.py

--- custom_components/netthings/__init__.py
+++ custom_components/netthings/__init__.py
@@ -22,21 +22,59 @@ def setup(hass, config):
     def refresh(data):
         for channel in data:
             attributes = {
+                "device_class": "power",
+                "state_class": "measurement",
                 "unit_of_measurement": channel["consumptionUnit"],
                 "friendly_name": channel["channelName"],
                 "Channel ID" : str(channel["channelId"]),
+                "Channel Type" : channel["channelType"]
+#                "Current Rate" : channel["unitCostInPence"] + " p",
+#                "Current Cost" : "Â£ " + str(channel["currentCost"]),
+#                "Monthly Accumulated Cost" : "Â£ " + str(channel["monthlyAccumulatedCost"]),
+#                "Current Estimated CO2" : str(channel["CO2"]) + " Kg/h"
+            }
+            currentrate_attributes = {
+                "state_class": "measurement",
+                "device_class": "monetary",
+                "unit_of_measurement": channel["currency"],
+                "friendly_name": channel["channelName"] + " Current Rate",
+                "Channel ID" : str(channel["channelId"]),
                 "Channel Type" : channel["channelType"],
-                "Current Rate" : channel["unitCostInPence"] + " p",
-                "Current Cost" : "£ " + str(channel["currentCost"]),
-                "Monthly Accumulated Cost" : "£ " + str(channel["monthlyAccumulatedCost"]),
-                "Current Estimated CO2" : str(channel["CO2"]) + " Kg/h"
-                }
+                "Current Rate in Pence" : channel["unitCostInPence"] + " p"
+            }
+            currentcost_attributes = {
+                "state_class": "measurement",
+                "device_class": "monetary",
+                "unit_of_measurement": channel["currency"],
+                "friendly_name": channel["channelName"] + " Current Cost",
+                "Channel ID" : str(channel["channelId"]),
+                "Channel Type" : channel["channelType"]
+            }
+            cumulativecost_attributes = {
+                "state_class": "measurement",
+                "device_class": "monetary",
+                "unit_of_measurement": channel["currency"],
+                "friendly_name": channel["channelName"] + " Monthly Accumulated Cost",
+                "Channel ID" : str(channel["channelId"]),
+                "Channel Type" : channel["channelType"]
+            }
+            co2_attributes = {
+                "state_class": "measurement",
+                "unit_of_measurement": "Kg/h",
+                "friendly_name": channel["channelName"] + " Current Estimated CO2",
+                "Channel ID" : str(channel["channelId"]),
+                "Channel Type" : channel["channelType"]
+            }
             hass.states.set("sensor.netthings_channel" + str(channel["channelId"]), str(channel["consumption"]), attributes)
+            hass.states.set("sensor.netthings_channel" + str(channel["channelId"]) + "_CurrentRate", str(float(channel["unitCostInPence"])/100), currentrate_attributes)
+            hass.states.set("sensor.netthings_channel" + str(channel["channelId"]) + "_CurrentCost", str(channel["currentCost"]), currentcost_attributes)
+            hass.states.set("sensor.netthings_channel" + str(channel["channelId"]) + "_MonthlyAccumulatedCost", str(channel["monthlyAccumulatedCost"]), cumulativecost_attributes)
+            hass.states.set("sensor.netthings_channel" + str(channel["channelId"]) + "_EstimatedCO2", str(channel["CO2"]), co2_attributes)
     
     @sio.event
     def disconnect():
         _LOGGER.debug("disconnected from " + host)
-        
+    
     sio.connect(url)
 
     # Return boolean to indicate that initialization was successfully.
